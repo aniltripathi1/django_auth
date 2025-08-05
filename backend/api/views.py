3b9d4e8fc8aef17370d488b3eafe5e55d9d464a7
aniltripathi1 <aniltripathi685@gmail.com>
2025-08-05 20:44:01 +0530

    Basic paeg update

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .serializers import UserSerializer 
+
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
